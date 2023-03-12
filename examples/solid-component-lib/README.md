8f9b43bd079b6b3b8d8a27335114b38108332a97
Zihan Chen <me@zihan.ga>
2023-03-12 11:58:44 +0800

    Update

---
+++ examples/solid-component-lib/README.md
@@ -0,0 +1,5 @@
+# solid-component-lib
+
+No description.
+
+Created with [Create Zihan Lib](https://github.com/zihan-ch/create-zihan-lib)
---
+++ examples/solid-component-lib/vite.config.ts
@@ -0,0 +1,33 @@
+import { defineConfig } from "vite"
+import AutoImport from "unplugin-auto-import/vite"
+import Dts from "vite-plugin-dts"
+import Solid from "vite-plugin-solid"
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: "./src/index.tsx",
+			formats: ["es"],
+			fileName: "index",
+		},
+		minify: false,
+		rollupOptions: {
+			external: [/^solid-js/gi],
+		},
+	},
+	plugins: [
+		AutoImport({
+			dts: "./src/auto-imports.generated.d.ts",
+			imports: [
+				"solid-js",
+				{
+					"solid-use/atom": [["default", "atom"]],
+				},
+			],
+			dirs: ["src/components"],
+			defaultExportByFilename: true,
+		}),
+		Dts(),
+		Solid(),
+	],
+})
